3d74d91f1b46c76b7f3d67e48812f1920223ff4d
unknown <0505943279@ts2761.2t.local>
2015-02-02 16:38:48 +0000

    added included files

---
+++ includes/header.inc.php
@@ -0,0 +1,33 @@
+<main class="hero">
+    <div class="container">
+
+        <div class="hero-title">
+            <h1>Web album</h1>
+        </div>
+
+        <div class="hero-footer">
+
+            <div class="social-icons">
+                <a href="https://facebook.com/saethor94" target="_blank" class="social-icon">
+                    <i class="fa fa-facebook"></i>
+                </a>
+                <a href="https://twitter.com/saethor94" target="_blank" class="social-icon">
+                    <i class="fa fa-twitter"></i>
+                </a>
+                <a href="https://plus.google.com/u/0/+S%C3%A6%C3%BE%C3%B3rHallgr%C3%ADmsson/" target="_blank" class="social-icon">
+                    <i class="fa fa-google-plus"></i>
+                </a>
+                <a href="https://github.com/saethor" target="_blank" class="social-icon">
+                    <i class="fa fa-github"></i>
+                </a>
+            </div>
+
+            <div class="scroll">
+                <a href="#section-album"><i class="fa fa-angle-double-down"></i></a>
+            </div>
+
+        </div>
+
+    </div>
+</main>
+<!-- /.Hero -->
---
+++ includes/nav.inc.php
@@ -0,0 +1,33 @@
+<div class="navbar-wrapper">
+    <div class="container">
+        <nav class="navbar navbar-default">
+            <div class="container">
+
+                <div class="navbar-header">
+                    <button type="button" class="toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+                        <span class="sr-only">Toggle navigation</span>
+                            Menu
+                    </button>
+                    <a class="navbar-brand" href="#">Photobase</a>
+                </div>
+
+                <div id="navbar" class="navbar-collapse collapse navbar-right">
+                    <ul class="nav navbar-nav">
+                        <li class="active">
+                            <a href="/">Home</a>
+                        </li>
+                        <li>
+                            <a href="/albums">Albums</a>
+                        </li>
+                        <li>
+                            <a href="#contact">Contact</a>
+                        </li>
+                    </ul>
+
+                </div>
+
+
+            </div>
+        </nav>
+    </div>
+</div>
