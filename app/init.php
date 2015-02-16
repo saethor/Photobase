3b450b33b1ba7eac3a33adea72f6495347536f8d
Sæþór <saethor94@gmail.com>
2015-02-16 13:41:01 +0000

    Removed cachelimit

--- app/init.php
+++ app/init.php
@@ -7,7 +7,3 @@ $path = "/2t/0505943279/";
 $title = basename(dirname($_SERVER['PHP_SELF']));
 $title = str_replace('_', ' ', $title);
 $title = ucwords($title);
-
-header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
-header("Cache-Control: post-check=0, pre-check=0", false);
-header("Pragma: no-cache");
