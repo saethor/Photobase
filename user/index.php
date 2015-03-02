42062e5ceecf268a91d18fe14fb24176d1bac1a0
Sæþór <saethor94@gmail.com>
2015-03-02 16:44:26 +0000

    Tabmenu with hardcoded filter ready

--- user/index.php
+++ user/index.php
@@ -27,19 +27,50 @@ require_once '../includes/session_timeout.php';
 
     <section class="section-photos" id="section-photos">
 
-        <div class="row">
+        <div class="row" role="tabpanel">
             <h2 class="section-title">Nýjustu myndirnar þínar</h2>
-
-            <?php foreach ($db_man->imageList() as $key => $value): ?>
-                <div class="col-xs-6 col-sm-4 col-md-3 item">
-                    <a href="<?php echo $path; ?>assets/images/<?php echo $db_man->getImageInfo($value[0])[2]; ?>">
-                        <img src="<?php echo $path; ?>assets/images/<?php echo $db_man->getImageInfo($value[0])[2]; ?>" alt="">
-                    </a>
-                </div>
-            <?php endforeach; ?>
-
             
+            <!-- Nav tabs -->
+            <ul class="nav nav-tabs" role="tablist">
+                <li role="presentation" class="active">
+                    <a href="#People" aria-controls="People" role="tab" data-toggle="tab">People</a>
+                </li>
+                <li role="presentation">
+                    <a href="#Landscapes" aria-controls="Landscapes" role="tab" data-toggle="tab">Landscapes</a>
+                </li>
+            </ul>
 
+            <!-- Tab panes -->
+            <div class="tab-content">
+
+                <!-- Tab for all the images in the people catagory -->
+                <div role="tabpanel" class="tab-pane active" id="People">
+                    <?php foreach ($db_man->imageList() as $key => $value): 
+                        if ($value[2] == "People"): ?>
+
+                        <div class="col-xs-6 col-sm-4 col-md-3 item">
+                            <a href="<?php echo $path; ?>assets/images/<?php echo $db_man->getImageInfo($value[0])[2]; ?>">
+                                <img src="<?php echo $path; ?>assets/images/<?php echo $db_man->getImageInfo($value[0])[2]; ?>" alt="">
+                            </a>
+                        </div> 
+
+                    <?php endif; endforeach; ?>
+                </div>
+                
+                <!-- Tab for all the images in the landscape catagory -->
+                <div role="tabpanel" class="tab-pane" id="Landscapes">
+                <?php foreach ($db_man->imageList() as $key => $value): 
+                    if ($value[2] == "Landscapes"): ?>
+
+                    <div class="col-xs-6 col-sm-4 col-md-3 item">
+                        <a href="<?php echo $path; ?>assets/images/<?php echo $db_man->getImageInfo($value[0])[2]; ?>">
+                            <img src="<?php echo $path; ?>assets/images/<?php echo $db_man->getImageInfo($value[0])[2]; ?>" alt="">
+                        </a>
+                    </div> 
+
+                <?php endif; endforeach;?>
+                </div>
+            </div>
         </div>
 
     </section>
