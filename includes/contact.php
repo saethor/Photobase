56b623b34b214ebdd4e64783ffa1b71e1b99ad5a
Saethor <saethor94@gmail.com>
2015-03-23 16:07:18 +0000

    Added contact form to a seperate include file

---
+++ includes/contact.php
@@ -0,0 +1,25 @@
+<section class="section-contact" id="section-contact">
+
+    <div class="row">
+        <h2 class="section-title">Hafðu samband</h2>
+
+        <form class="contact">
+            <div class="col-md-6 form-group">
+                <input type="text" id="name" class="form-control">
+                <label for="name">Your name</label>
+            </div>
+            <div class="col-md-6 form-group">
+                <input type="email" id="email" class="form-control">
+                <label for="email">Your email</label>
+            </div>
+            <div class="col-md-12 form-group">
+                <textarea id="message" class="form-control" rows="10"></textarea>
+                <label for="message">Your message</label>
+            </div>
+            <div class="col-md-12 form-group">
+                <input type="submit" class="form-control">
+            </div>
+
+        </form>
+    </div>
+</section>
--- index.php
+++ index.php
@@ -26,35 +26,9 @@ require_once 'app/init.php';
 
     <?php include 'includes/hero.inc.php'; ?>
 
-    <section class="section-contact" id="section-contact">
+    <?php include 'includes/contact.php'; ?>
 
-        <div class="row">
-            <h2 class="section-title">Hafðu samband</h2>
-
-            <form class="contact">
-                <div class="col-md-6 form-group">
-                    <input type="text" id="name" class="form-control">
-                    <label for="name">Your name</label>
-                </div>
-                <div class="col-md-6 form-group">
-                    <input type="email" id="email" class="form-control">
-                    <label for="email">Your email</label>
-                </div>
-                <div class="col-md-12 form-group">
-                    <textarea id="message" class="form-control" rows="10"></textarea>
-                    <label for="message">Your message</label>
-                </div>
-                <div class="col-md-12 form-group">
-                    <input type="submit" class="form-control">
-                </div>
-
-            </form>
-        </div>
-
-    </section>
-
-    <?php include 'includes/footer.inc.php';
-    ?>
+    <?php include 'includes/footer.inc.php'; ?>
 
     <!-- Bootstrap core JavaScript
     ================================================== -->
