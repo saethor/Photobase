1f6f069c8c3f963987362b4c7c4ab45a277fcc0c
Saethor <saethor94@gmail.com>
2015-04-15 08:43:18 +0000

    Navbar responsive does not display empty menu in mobile

--- includes/nav.inc.php
+++ includes/nav.inc.php
@@ -10,35 +10,31 @@ $dirname = basename(dirname($_SERVER['PHP_SELF']));
 
 
                 <div class="navbar-header">
+
+                <?php if (isset($_SESSION['user_id'])): ?>
                     <button type="button" class="toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
                         <span class="sr-only">Toggle navigation</span>
                         Menu
                     </button>
+                <?php endif ?>
+
                     <a class="navbar-brand" href="<?php echo $path ?>">Photobase</a>
                 </div>
 
+                <?php if (isset($_SESSION['user_id'])): ?>
 
-                <div id="navbar" class="navbar-collapse collapse navbar-right">
+                <div id="navbar" class="navbar-collapse collapse">
 
-                    <ul class="nav navbar-nav">
-                        <?php if (isset($_SESSION['user_id'])): ?>
-                            <li>
-                                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false"><?= $db_man->getUser($_SESSION['user_id'])[4]; ?> <span class="caret"></span></a>
-                                <ul class="dropdown-menu" role="menu">
-                                    <li>
-                                        <a href="<?php echo $path . 'users/' ?>">Profile</a>
-                                    </li>
-                                    <li>
-                                        <a href="<?php echo $path . 'includes/logout.php'; ?>">Logout</a> 
-                                    </li>
-                                </ul>
-                            </li>
-                        <?php endif ?>
+                    <ul class="nav navbar-nav navbar-right">
+                        <li>
+                            <a href="<?php echo $path . 'users/' ?>">Profile</a>
+                        </li>
+                        <li>
+                            <a href="<?php echo $path . 'includes/logout.php'; ?>">Logout</a> 
+                        </li>
                     </ul>
-
                 </div>
-
-
+                <?php endif ?>
             </div>
         </nav>
     </div>
